fea3beefd6a620fccad3fbabebb26f425dfe4270
deven <devendra.jha@flex.com>
2020-05-12 12:02:16 +0530

    API created

--- src/main/java/shoppingCart/web/LoginController.java
+++ src/main/java/shoppingCart/web/LoginController.java
@@ -1,18 +1,19 @@
 package shoppingCart.web;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+import org.apache.http.protocol.HTTP;
+import org.codehaus.jettison.json.JSONObject;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,45 +24,80 @@ import org.springframework.web.bind.annotation.RestController;
 import shoppingCart.dao.LoginDao;
 import shoppingCart.model.LoginBean;
 
+@WebServlet("/login")
 
-@RestController
-@RequestMapping(value = "/dologin")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private LoginDao loginDao;
-	
+
 	Map<Integer, LoginBean> LoginDetails = new HashMap<Integer, LoginBean>();
 
 	public void init() {
 		loginDao = new LoginDao();
 	}
-	@RequestMapping(value = "/validate", method = RequestMethod.GET)
-	public @ResponseBody LoginBean getLoginDetails(HttpServletRequest request, HttpServletResponse response) throws IOException {
-			response.sendRedirect("login/login.jsp");
-			LoginBean loginBean = new LoginBean();
-			return loginBean;	
-	}
-	
-	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public @ResponseBody LoginBean createUser(HttpServletRequest request, HttpServletResponse response, @RequestBody LoginBean loginBean) throws ServletException, IOException {
-		
-		
-		return loginBean;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// response.sendRedirect("login/login.jsp");
+		String name = request.getParameter("username");
+		String pwd = request.getParameter("password");
+		LoginBean loginBean = new LoginBean();
+		loginBean.setUsername(name);
+		loginBean.setPassword(pwd);
+		LoginDao dao = new LoginDao();
+		boolean str = false;
+		try {
+			str = dao.validate(loginBean);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(str);
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println(str);
 	}
 
-	@RequestMapping(value = "/updatepassword", method = RequestMethod.PUT)
-	public @ResponseBody LoginBean updateUser(@PathVariable("id") int LoginId, HttpServletRequest request, HttpServletResponse response, @RequestBody LoginBean loginBean) throws ServletException, IOException {
-		LoginBean loginBean1 = LoginDetails.get(LoginId);
-		LoginDetails.remove(LoginId);
-		return loginBean1;
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null)
+				jb.append(line);
+		} catch (Exception e) {
+			/* report an error */ 
+		}
+		
+		
+		System.out.println(jb);
+
 	}
-	
-	@RequestMapping(value = "/deleteUser", method = RequestMethod.DELETE)
-	public @ResponseBody LoginBean deleteUser(@PathVariable("id") int LoginId, HttpServletRequest request, HttpServletResponse response, @RequestBody LoginBean loginBean) throws ServletException, IOException {
-		LoginBean loginBean1 = LoginDetails.get(LoginId);
-		LoginDetails.remove(LoginId);
-		return loginBean1;
-	}
-	
-	
+	/*
+	 * @RequestMapping(value = "/signup", method = RequestMethod.POST)
+	 * public @ResponseBody LoginBean createUser(HttpServletRequest request,
+	 * HttpServletResponse response, @RequestBody LoginBean loginBean) throws
+	 * ServletException, IOException {
+	 * 
+	 * 
+	 * return loginBean; }
+	 * 
+	 * @RequestMapping(value = "/updatepassword", method = RequestMethod.PUT)
+	 * public @ResponseBody LoginBean updateUser(@PathVariable("id") int LoginId,
+	 * HttpServletRequest request, HttpServletResponse response, @RequestBody
+	 * LoginBean loginBean) throws ServletException, IOException { LoginBean
+	 * loginBean1 = LoginDetails.get(LoginId); LoginDetails.remove(LoginId); return
+	 * loginBean1; }
+	 * 
+	 * @RequestMapping(value = "/deleteUser", method = RequestMethod.DELETE)
+	 * public @ResponseBody LoginBean deleteUser(@PathVariable("id") int LoginId,
+	 * HttpServletRequest request, HttpServletResponse response, @RequestBody
+	 * LoginBean loginBean) throws ServletException, IOException { LoginBean
+	 * loginBean1 = LoginDetails.get(LoginId); LoginDetails.remove(LoginId); return
+	 * loginBean1; }
+	 * 
+	 */
 }
